ea2aa489e60ca5988e5977d3ee3264d07d28adca
Sylvain Oliver <50275847+syoliver@users.noreply.github.com>
2024-06-08 22:51:03 +0200

    Add PR CI workflow

--- env.bat
+++ env.bat
@@ -1,2 +1,9 @@
-docker build -t device-sdk-rust-build-env --target build_env .
+FOR /F "eol=# tokens=*" %%i IN (%~dp0.env) DO SET %%i
+
+docker build                                        ^
+    -t device-sdk-rust-build-env                    ^
+    --target build_env                              ^
+    --build-arg "ALPINE_VERSION=%ALPINE_VERSION%"   ^
+    --build-arg "RUST_VERSION=%RUST_VERSION%"       ^
+    .
 docker run --rm -v %CD%:/src -it device-sdk-rust-build-env /bin/sh
--- generate_api.bat
+++ generate_api.bat
@@ -1,11 +1,12 @@
 
-set EDGEX_VERSION=v3.2.0-dev.12
+FOR /F "eol=# tokens=*" %%i IN (%~dp0.env) DO SET %%i
+
 curl --output device-sdk.yaml https://raw.githubusercontent.com/edgexfoundry/device-sdk-go/%EDGEX_VERSION%/openapi/v3/device-sdk.yaml
-docker run --rm -v %CD%:/local openapitools/openapi-generator-cli:latest ^
-    generate ^
-    -i /local/device-sdk.yaml ^
-    -t /local/tools/generator ^
-    -g rust-axum ^
-    -c /local/openapi_config.yaml ^
-    -o /local/external/openapi ^
+docker run --rm -v %CD%:/local openapitools/openapi-generator-cli:latest    ^
+    generate                                                                ^
+    -i /local/device-sdk.yaml                                               ^
+    -t /local/tools/generator                                               ^
+    -g rust-axum                                                            ^
+    -c /local/openapi_config.yaml                                           ^
+    -o /local/external/openapi                                              ^
     --generate-alias-as-model
